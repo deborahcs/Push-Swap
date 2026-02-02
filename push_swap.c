8f966a71259b95295f39392bd4215a6b2803ca8d
decabral <deborahcabralsilva@gmail.com>
2026-02-02 16:57:22 -0300

    changes in swap.c and .h

--- push_swap.c
+++ push_swap.c
@@ -6,10 +6,21 @@
 /*   By: decabral <decabral@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 10:52:46 by decabral          #+#    #+#             */
-/*   Updated: 2026/01/23 10:59:30 by decabral         ###   ########.fr       */
+/*   Updated: 2026/02/02 16:01:53 by decabral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
 
+	a = NULL;
+	b = NULL;
+	if (argc == 1 || argc == 2 && !argv[1][0])
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
+}
--- push_swap.h
+++ push_swap.h
@@ -6,14 +6,29 @@
 /*   By: decabral <decabral@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/23 10:53:50 by decabral          #+#    #+#             */
-/*   Updated: 2026/01/23 15:54:45 by decabral         ###   ########.fr       */
+/*   Updated: 2026/02/02 15:53:40 by decabral         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include <stdbool.h> 
+# include <limits.h>
 # include
-# include
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;	
+}	t_stack_node;
+
 
 
 
